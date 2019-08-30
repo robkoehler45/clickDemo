360dcc799f8ff54f5636a5d2a3030bc4c722ef73
Rob Koehler <robkoehler45@gmail.com>
2019-08-30 10:29:11 -0500

    new

--- scripts.js
+++ scripts.js
@@ -12,11 +12,12 @@ function makeid(length) {
 function showClickWrapper() {
   document.getElementById ("start").style.display = "none";
   document.getElementById("spin").sytle.display = "table";
+  var x = makeid(12);
   docuSignClick.Clickwrap.render({
       environment: 'https://demo.docusign.net',
       accountId: '53e87a81-3ab4-43d4-9d29-b7861bfc1e1e',
       clickwrapId: '8d6ef161-0398-40d7-be90-03136af40036',
-      clientUserId: makeid(12),
+      clientUserId: x,
       format: 'modal',
       onAgreed: callbackFnAgreed,
 			onMustAgree: callbackFnMustAgree,
