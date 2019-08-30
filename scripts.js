2c90ada685579d652b1e10429afc47a393d27ccf
Rob Koehler <robkoehler45@gmail.com>
2019-08-30 10:24:21 -0500

    more changes

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ function makeid(length) {
    for ( var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
    }
+   document.getElementById("userId").value = result;
    return result;
 }
 
@@ -22,3 +23,23 @@ function showClickWrapper() {
 			onError: callbackFnError
     }, '#ds-terms-of-service');
 }
+
+function callbackFnAgreed(){
+		console.log ("callbackFnAgreed");
+		//Do something
+		document.getElementById ('signUpForm').style.display = "none";
+		document.getElementById ('ds-terms-of-service').style.display = "none";
+		document.getElementById ('signUpFormAgreed').style.display = "block";
+		document.getElementById ('cochref').href = "https://demo.docusign.net/clickapi/v1/accounts/53e87a81-3ab4-43d4-9d29-b7861bfc1e1e/clickwraps/8d6ef161-0398-40d7-be90-03136af40036/users/" + document.getElementById ('userId').value + "/documents/combined";
+	}
+
+	function callbackFnMustAgree(e){
+		console.log ("callbackFnMustAgree");
+		document.getElementById ("spin").style.display = "none";
+		//Do something
+	}
+
+	function callbackFnError(e){
+		console.log ("callbackFnError");
+		//Do something
+	}
