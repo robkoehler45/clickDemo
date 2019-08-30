69573959f0a9d2ef2b47e6411619b259dcd36c22
Rob Koehler <robkoehler45@gmail.com>
2019-08-30 09:49:10 -0500

    moving scripts to js

--- scripts.js
+++ scripts.js
@@ -7,3 +7,15 @@ function makeid(length) {
    }
    return result;
 }
+
+function showClickWrapper() {
+  document.getElementById ("start").style.display = "none";
+  document.getElementById("ds-terms-of-service").sytle.display = "block";
+  docuSignClick.Clickwrap.render({
+      environment: 'https://demo.docusign.net',
+      accountId: '53e87a81-3ab4-43d4-9d29-b7861bfc1e1e',
+      clickwrapId: '8d6ef161-0398-40d7-be90-03136af40036',
+      clientUserId: makeid(12),
+      format: 'modal'
+    }, '#ds-terms-of-service');
+}
