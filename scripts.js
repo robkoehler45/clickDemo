1af7c3515889520eaa3f0f42dad9bd6009b78b75
Rob Koehler <robkoehler45@gmail.com>
2019-08-31 10:26:40 -0500

    updates

--- scripts.js
+++ scripts.js
@@ -31,7 +31,7 @@ function callbackFnAgreed(){
 		document.getElementById ('signUpForm').style.display = "none";
 		document.getElementById ('ds-terms-of-service').style.display = "none";
 		document.getElementById ('signUpFormAgreed').style.display = "block";
-		document.getElementById ('cochref').href = "https://appdemo.docusign.com/clickwrap/8d6ef161-0398-40d7-be90-03136af40036/responses?search=" + document.getElementById ('userId').value;
+		document.getElementById ('cochref').href = "https://appdemo.docusign.com/clickwrap/8d6ef161-0398-40d7-be90-03136af40036/responses";
 	}
 
 	function callbackFnMustAgree(e){
